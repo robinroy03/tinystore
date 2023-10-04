3809aa1ee901718e98c848cd29175f06bde706b8
Robin Roy <robinroy.work@gmail.com>
2023-10-04 10:59:29 +0530

    main.c

--- main.c
+++ main.c
@@ -9,6 +9,30 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum {
+  EXECUTE_SUCCESS,
+  EXECUTE_DUPLICATE_KEY,
+} ExecuteResult;
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT, 
+    STATEMENT_SELECT 
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
     input_buffer -> buffer = NULL;
@@ -16,6 +40,45 @@ InputBuffer* new_input_buffer() {
     input_buffer -> input_length = 0;
 
     return input_buffer;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer -> buffer);
+    free(input_buffer);
+}
+
+// meta commands are non-sql statements, they all start with a dot
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer -> buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer -> buffer, "insert", 6) == 0) {
+        statement -> type = STATEMENT_INSERT;
+        return PREPARE_SUCESS;
+    }
+    if (strcmp(input_buffer -> buffer, "select") == 0) {
+        statement -> type = STATEMENT_SELECT;
+        return PREPARE_SUCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement -> type) {
+        case (STATEMENT_INSERT):
+            printf("This is where we would do an insert\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("This is where we would do a select\n");
+            break;
+    }
 }
 
 void print_prompt() {
@@ -39,23 +102,32 @@ void read_input(InputBuffer* input_buffer) {
     input_buffer -> buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer -> buffer);
-    free(input_buffer);
-}
-
 int main(void) {
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer -> buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+        if (input_buffer -> buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command %s\n", input_buffer -> buffer);
+                    continue;
+            }
         }
-        else {
-            printf("Unrecognized command '%s'.\n", input_buffer -> buffer);
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized statement at start of %s\n", input_buffer -> buffer);
+                continue;
         }
+
+        execute_statement(&statement);
+        printf("Executed");
     }
 }
